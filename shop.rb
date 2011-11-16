0732eb6f55e56a5255dfd38960dc1157e75cdccc
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2011-11-16 13:32:46 +0100

    Added rvm and Bundler support + initial Capybara integration.

--- shop.rb
+++ shop.rb
@@ -1,5 +1,29 @@
+require 'capybara'
+require 'capybara/dsl'
+require "selenium-webdriver"
+
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://localhost:4567/kaos/'
+Capybara.run_server = false
+
+# This seems to be the default...
+#Capybara.register_driver :selenium do |app|
+#  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+#end
+
 class Shop
+  
+  def initialize
+    Capybara.visit("http://www.google.com/")
+    super()
+  end
+  
   state_machine :initial => :anonymous do
+    
+    after_transition :on => :add_to_cart do
+      Capybara.visit("http://www.apple.com/")
+    end
+    
     event :add_to_cart do
       transition :anonymous  => :anonymous
     end
